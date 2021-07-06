4cd5e08f782b0a75b3d058aa5985b7ae04691a61
mgbarois <mgbarois@gmail.com>
2021-07-06 13:21:03 +0200

    logo changes

--- src/components/Navigation/Logo/Logo.js
+++ src/components/Navigation/Logo/Logo.js
@@ -7,7 +7,7 @@ import icon from './eye-solid-square.png';
 const Logo = (props) =>  {
     return (
         <div>
-            <Tilt className="Tilt shadow-5" options={{ max : 65 }} style={{ height: 90, width: 90 }} >
+            <Tilt className="Tilt shadow-5" options={{ max : 65 }} style={{ height: 110, width: 110 }} >
                 <div className="Tilt-inner pa3"> <img src={icon} alt='icon'/></div>
             </Tilt>
         </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,8 +6,8 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
         <nav className='ma4 mt0' style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', height: 120}} width='100%'>
             <Logo/>
             { isSignedIn 
-                ? <span className='f5 link dim br1 ba ph3 pv2 mb2 dib black' onClick={() => onRouteChange('signin')}>Sign Out</span>                 
-                : <div className="f5 link dim br1 ba ph3 pv2 mb2 dib black">
+                ? <span className='f5 link dim br1 ba ph3 pv2 mb2 dib black shadow-5' onClick={() => onRouteChange('signin')}>Sign Out</span>                 
+                : <div className="f5 link dim br1 ba ph3 pv2 mb2 dib black shadow-5">
                     <span className='f5 link br1 dim black pa1 pointer' onClick={() => onRouteChange('signin')}>Sign In </span>| 
                     <span className='f5 link dim black pa1 pointer' onClick={() => onRouteChange('register')}>Register</span>
                   </div>
